dee8f3119d3718370120838865920dbff2dd2ce5
RogerAlbuquerqueBR <contatorogeroficial@gmail.com>
2024-07-24 22:20:30 -0300

    Initial commit

---
+++ Transportes/Controllers/VeiculosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transportes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VeiculosController : ControllerBase
+    {
+        
+    }
+}
---
+++ Transportes/WeatherForecast.cs
@@ -0,0 +1,6 @@
+namespace Transportes
+{
+    public class Veiculos
+    {
+    }
+}
